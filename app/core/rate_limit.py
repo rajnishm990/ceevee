0941ff297f9344266c8d9191d86fc12d3f53cadd
rajnishm990 <rajnishm990@gmail.com>
2026-09-07 13:01:37 +0530

    add rate limiting

---
+++ app/core/rate_limit.py
@@ -0,0 +1,12 @@
+from slowapi import Limiter 
+from slowapi.util import get_remote_address
+from app.core.config import settings 
+
+limiter  = Limiter(
+    key_func=get_remote_address,
+    storage_uri=settings.REDIS_URL,
+    strategy="moving-window",
+    default_limits=["100/minute"]
+
+)
+
